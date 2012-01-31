539524ed8e70613974b4beffad59e0ab8651821b
carl-ellis <Carlc75@gmail.com>
2012-01-31 17:16:53 +0000

    iRemoved the string restriction - pointless if you only use strings ....
    Signed-off-by: carl-ellis <Carlc75@gmail.com>

--- jserialisable.rb
+++ jserialisable.rb
@@ -12,7 +12,7 @@ class Class
 
 		#Build to the to_json method
 		tojson = "def to_json(*a)\n{ 'json_class' => #{self.name}"
-		attrs.each { |a| tojson << ",\n'#{a}' => @#{a}.to_s"}
+		attrs.each { |a| tojson << ",\n'#{a}' => @#{a}"}
 		tojson << "\n}.to_json(*a)\nend"
 
 		#build to json_create method
